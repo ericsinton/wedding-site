ad1d228d6bcadbf8479df2b8d048b2cc401a504b
Eric Sinton <ericsinton@gmail.com>
2026-08-05 22:03:47 -0400

    Add contact note to Where to Stay section for out-of-town guests

--- app/travel/page.tsx
+++ app/travel/page.tsx
@@ -61,6 +61,8 @@ const transitItems: TransitItem[] = [
       <a href="https://www.cambriaplymouth.com" target="_blank" rel="noopener noreferrer">Cambria Hotel</a>, the{' '}
       <a href="https://www.hilton.com/en/hotels/pymhshx-hampton-suites-plymouth" target="_blank" rel="noopener noreferrer">Hampton Inn and Suites</a>, and the{' '}
       <a href="https://www.marriott.com/en-us/hotels/ewbpf-fairfield-inn-and-suites-plymouth/overview/" target="_blank" rel="noopener noreferrer">Fairfield by Marriott Inn and Suites</a> in Plymouth.
+      <br /><br />
+      If you are coming from out of town and want to connect with us about staying with one of our nearby friends or family, or otherwise have any questions about where to stay, please contact Eric or Kate directly by email (<a href="mailto:ericsinton@gmail.com">ericsinton@gmail.com</a> or <a href="mailto:k8lamberti@gmail.com">k8lamberti@gmail.com</a>).
     </>,
   },
 ]
